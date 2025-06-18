137670a887fde7fa15fd188a536ec914d7405cf2
guoping.liu <guoping.liu@3reality.com>
2025-06-18 11:03:42 +0800

    update w1ss1 driver

---
+++ drivers/net/wireless/w1ss1/vmac/print_version.c
@@ -0,0 +1,6 @@
+#include "wifi_hal_com.h"
+
+void print_driver_version(void) {
+    printk("driver compile date: 2021-12-24 18:32:09,driver hash: 33f8d79eac54ffddd3e2a4c92f50903e91b78580\n");
+    printk("fw compile date: 2021-12-24 18:26:51,fw hash: ,fw size: 1071764\n");
+}
