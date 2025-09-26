28ea321fd32c4a416040449c68a18885bb9d6987
guoping.liu <guoping.liu@3reality.com>
2025-09-26 11:07:01 +0800

    Add Amlogic W155S1 driver

---
+++ drivers/net/wireless/w1/vmac/print_version.c
@@ -0,0 +1,6 @@
+#include "wifi_hal_com.h"
+
+void print_driver_version(void) {
+    printk("driver compile date: 2022-09-01 14:51:41,driver hash: b0c8817049f03570708455c438aac30b4c522438\n");
+    printk("fw compile date: 2022-07-19 11:35:03,fw hash: ,fw size: 1071764\n");
+}
